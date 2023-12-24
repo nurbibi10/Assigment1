059a7cfd1757a9da848df07e3ccfc34f31bd5c7e
nurbibi10 <231645@astabait.edu.kz>
2023-12-24 18:04:51 +0600

    wrote a function that defines points

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,4 +1,37 @@
 package models;
 
 public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+
+    public double distance(Point point) {
+        double xLen = Math.pow(point.getX() - this.x, 2);
+        double yLen = Math.pow(point.getY() - this.y, 2);
+        return Math.sqrt(xLen + yLen);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String toString() {
+        return "Point ( x=" + x + ", y=" + y + " )";
+    }
 }
