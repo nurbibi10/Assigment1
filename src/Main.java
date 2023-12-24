772e24547ca8165b9923713410b3207095ff9c19
nurbibi10 <231645@astabait.edu.kz>
2023-12-24 17:42:14 +0600

    update the file, add class

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,13 @@ import models.Shape;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+    public class Main {
+        public static void main(String[] args) throws FileNotFoundException {
+            File file = new File("C:\\\\Users\\\\User\\\\IdeaProjects\\\\untitled\\\\src\\\\source.txt");
+            Shape shape = readShapeFromFile(file);
+            System.out.println("The perimeter: " + shape.calculatePerimeter());
+            System.out.println("The longest side: " + shape.getLongestSide());
+            System.out.println("The average side: " + shape.getAvgSide());
+        }
+
