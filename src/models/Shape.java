262e583e1a1b0b98242087067160774404685b53
nurbibi10 <231645@astabait.edu.kz>
2023-12-24 18:13:38 +0600

    read the coordinates

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,4 +1,19 @@
 package models;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Shape {
+
+    private ArrayList<Point> points = new ArrayList<Point>();
+
+    public Shape() {
+        points = new ArrayList<>();
+    }
+
+    public void addPoint(Point point) {  // This method adds a point to the points array.
+        points.add(point);
+    }
+
 }
