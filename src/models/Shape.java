b6d1c2d51e3f955e299d7c24089412edcba7b559
nurbibi10 <231645@astabait.edu.kz>
2023-12-24 18:18:14 +0600

    find the longest side

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -26,4 +26,17 @@ public class Shape {
         return perimeter;
     }
 
+    public double getAvgSide() {
+        return calculatePerimeter() / points.size();
+    }
+
+    public double getLongestSide() {  // This method returns the longest side of the polygon
+        int len = points.size();
+        double maxSide = points.get(len - 1).distance(points.getFirst());
+        for (int i = 0; i < len - 1; i++) {
+            double distance = points.get(i).distance(points.get(i+1));
+            if (distance > maxSide) maxSide = distance;
+        }
+        return maxSide;
+    }
 }
